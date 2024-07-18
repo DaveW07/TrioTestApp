841d8bd5e1a3f9f66f4918e228e369fdb1fbd24e
DaveW07 <david.wilkinson@astechprojects.co.uk>
2024-07-18 14:09:52 +0100

    Add files via upload

---
+++ TrioTestApp/TrioComms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrioPCLib;
+
+
+namespace TrioTestApp
+{
+    public class TrioComms
+    {
+
+        private TrioPC _motionController;
+        private int _etherCATNode;
+        private short _axisID;
+
+        /// <summary>
+        /// Constructor for Trio hardware
+        /// </summary>
+        /// <param name="motionController">The motion controller.</param>
+        /// <param name="etherCATNode">The ether cat node.</param>
+        public TrioComms(TrioPC motionController, int etherCATNode)
+        {
+            _motionController = motionController;
+            _etherCATNode = etherCATNode;
+            _axisID = (short)_etherCATNode;
+        }
+
+        public bool Connect()
+        {
+           return _motionController.Open(1, 1);
+        }
+
+}
+}
