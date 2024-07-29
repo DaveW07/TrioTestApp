1385fabad8e30fa47b0978c29b7799832e2e27e7
David Wilkinson <david.wilkinson@astechprojects.co.uk>
2024-07-29 11:22:24 +0100

    Updates for Task 66: Add Run Program Function

--- TrioTestApp/Form1.Designer.cs
+++ TrioTestApp/Form1.Designer.cs
@@ -29,8 +29,13 @@ namespace TrioTestApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnConnect = new System.Windows.Forms.Button();
             this.lblConnected = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cboPrograms = new System.Windows.Forms.ComboBox();
+            this.lblprogName = new System.Windows.Forms.Label();
+            this.progTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // btnConnect
@@ -56,15 +61,56 @@ namespace TrioTestApp
             this.lblConnected.TabIndex = 1;
             this.lblConnected.Text = "Not Connected";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(25, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run Program";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cboPrograms
+            // 
+            this.cboPrograms.FormattingEnabled = true;
+            this.cboPrograms.Items.AddRange(new object[] {
+            "Home",
+            "Shake",
+            "Fire"});
+            this.cboPrograms.Location = new System.Drawing.Point(169, 114);
+            this.cboPrograms.Name = "cboPrograms";
+            this.cboPrograms.Size = new System.Drawing.Size(105, 21);
+            this.cboPrograms.TabIndex = 3;
+            // 
+            // lblprogName
+            // 
+            this.lblprogName.AutoSize = true;
+            this.lblprogName.Location = new System.Drawing.Point(31, 173);
+            this.lblprogName.Name = "lblprogName";
+            this.lblprogName.Size = new System.Drawing.Size(70, 13);
+            this.lblprogName.TabIndex = 4;
+            this.lblprogName.Text = "Running: Idle";
+            // 
+            // progTimer
+            // 
+            this.progTimer.Interval = 3000;
+            this.progTimer.Tick += new System.EventHandler(this.progTimer_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(494, 217);
+            this.Controls.Add(this.lblprogName);
+            this.Controls.Add(this.cboPrograms);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblConnected);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
             this.Text = "Trio Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,6 +120,10 @@ namespace TrioTestApp
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Label lblConnected;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboPrograms;
+        private System.Windows.Forms.Label lblprogName;
+        private System.Windows.Forms.Timer progTimer;
     }
 }
 
--- TrioTestApp/Form1.cs
+++ TrioTestApp/Form1.cs
@@ -5,13 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TrioTestApp
 { 
     public partial class Form1 : Form
     {
+        bool _TrioConnected = false;
         
         public Form1()
         {
@@ -22,6 +23,28 @@ namespace TrioTestApp
         {
             MessageBox.Show("Trio Connected", "Trio Connection", MessageBoxButtons.OK);
             lblConnected.Text = "Connected";
+            _TrioConnected = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblConnected.Text = "Not Connected";
+            cboPrograms.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_TrioConnected && !progTimer.Enabled)
+            {
+                lblprogName.Text = "Running: " + cboPrograms.SelectedItem.ToString();
+                progTimer.Enabled = true;
+            }
+        }
+
+        private void progTimer_Tick(object sender, EventArgs e)
+        {
+            lblprogName.Text = "Running: Idle";
+            progTimer.Enabled = false;
         }
     }
 }
